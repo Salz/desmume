977865f74214a45b58519ee1a81ed5203da62aaa
Ingo Saitz <Ingo.Saitz@stud.uni-hannover.de>
2018-04-26 09:07:10 +0200

    LOCAL: configure added to autogen.sh

--- desmume/src/frontend/posix/autogen.sh
+++ desmume/src/frontend/posix/autogen.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 autoreconf --install --force --verbose
+
+# local configuration
+# depends: libagg-dev, liblua5.1-dev
+./configure --prefix=/home/ingo/stow/desmume.git --enable-gdb-stub --enable-osmesa --enable-glade --enable-wifi --enable-glx --enable-hud --enable-maintainer-mode
