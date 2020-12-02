0dce66e355706a337421ccfdc43bdc07c8543bf2
Ingo Saitz <Ingo.Saitz@stud.uni-hannover.de>
2020-12-02 10:56:53 +0100

    change to new build system
    with build.sh script in root dir

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -x
+set -e
+
+git clean -f -dx
+cd desmume/src/frontend/posix
+CFLAGS="-Ofast -march=native -DNDEBUG" CXXFLAGS="-Ofast -march=native -DNDEBUG" meson setup --prefix=/home/ingo/stow/desmume.git --buildtype=release build
+ninja -C build -v -j3
+ninja -C build install -v -j3
--- desmume/src/frontend/posix/autogen.sh
+++ desmume/src/frontend/posix/autogen.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
 autoreconf --install --force --verbose
-
-# local configuration
-# depends: libagg-dev, liblua5.1-dev
-CC=clang CXX=clang++ ./configure --prefix=/home/ingo/stow/desmume.git --enable-gdb-stub --enable-osmesa --enable-glade --enable-wifi --enable-glx --enable-hud --enable-maintainer-mode
