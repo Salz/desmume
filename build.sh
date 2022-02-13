e05ba5a4f3023e0ad876e54d36da7f613c342188
Ingo Saitz <Ingo.Saitz@stud.uni-hannover.de>
2022-02-13 22:32:10 +0100

    add wifi option in build

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ set -e
 
 git clean -f -dx
 cd desmume/src/frontend/posix
-CFLAGS="-Ofast -march=native -DNDEBUG" CXXFLAGS="-Ofast -march=native -DNDEBUG" meson setup --prefix=/home/ingo/stow/desmume.git --buildtype=release build
+CFLAGS="-Ofast -march=native -DNDEBUG" CXXFLAGS="-Ofast -march=native -DNDEBUG" meson setup -Dwifi=true --prefix=/home/ingo/stow/desmume.git --buildtype=release build
 ninja -C build -v -j3
 ninja -C build install -v -j3
