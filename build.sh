da2f4e11d6082ba1ffc2c9d0871f49b7a4ba74f1
Ingo Saitz <ingo@hannover.ccc.de>
2023-07-23 16:08:06 +0200

    automatically determine number of processing units

--- build.sh
+++ build.sh
@@ -3,8 +3,10 @@
 set -x
 set -e
 
+threads=$(($(nproc)+1))
+
 git clean -f -dx
 cd desmume/src/frontend/posix
 CFLAGS="-Ofast -march=native -DNDEBUG" CXXFLAGS="-Ofast -march=native -DNDEBUG" meson setup -Dwifi=true --prefix=/home/ingo/stow/desmume.git --buildtype=release build
-ninja -C build -v -j3
-ninja -C build install -v -j3
+ninja -C build -v -j$threads
+ninja -C build install -v -j$threads
