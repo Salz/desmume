f60eb7aef798b0bca68552cf3e83fc4d832e7125
Ingo Saitz <ingo@hannover.ccc.de>
2023-09-24 06:42:21 +0200

    add date and commit-hash to install dir

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 set -x
 set -e
 
-threads=$(($(nproc)+1))
+threads="$(($(nproc)+1))"
+version="$(date +%Y%m%d)-$(git rev-parse --short HEAD)"
 
 git clean -f -dx
 cd desmume/src/frontend/posix
-CFLAGS="-Ofast -march=native -DNDEBUG" CXXFLAGS="-Ofast -march=native -DNDEBUG" meson setup -Dwifi=true --prefix=/home/ingo/stow/desmume.git --buildtype=release build
-ninja -C build -v -j$threads
-ninja -C build install -v -j$threads
+CFLAGS="-Ofast -march=native -DNDEBUG" CXXFLAGS="-Ofast -march=native -DNDEBUG" meson setup -Dwifi=true --prefix="/home/ingo/stow/desmume-${version}" --buildtype=release build
+ninja -C build -v -j${threads}
+ninja -C build install -v -j${threads}
