8e92371d6e49b5b465f2f6791b692b1be62ff565
Ingo Saitz <ingo@hannover.ccc.de>
2024-08-13 06:32:06 +0200

    build.sh: new option opengl needed for opengl

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ version="$(date +%Y%m%d)-$(git rev-parse --short HEAD)"
 
 git clean -f -dx
 cd desmume/src/frontend/posix
-CFLAGS="-Ofast -march=native -DNDEBUG" CXXFLAGS="-Ofast -march=native -DNDEBUG" meson setup -Dwifi=true --prefix="/home/ingo/stow/desmume-${version}" --buildtype=release build
+CFLAGS="-Ofast -march=native -DNDEBUG" CXXFLAGS="-Ofast -march=native -DNDEBUG" meson setup -Dopengl=true -Dwifi=true --prefix="/home/ingo/stow/desmume-${version}" --buildtype=release build
 ninja -C build -v -j${threads}
 ninja -C build install -v -j${threads}
